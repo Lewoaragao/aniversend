b2ff66507fc0d6d7d08bc7bd668ea73a248985d4
Leonardo Aragão <lewoaragao@gmail.com>
2023-09-06 13:21:36 -0300

    Initial commit

---
+++ src/main/java/com/aniversend/AniversendApplication.java
@@ -0,0 +1,13 @@
+package com.aniversend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniversendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniversendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniversend/AniversendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniversend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniversendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
